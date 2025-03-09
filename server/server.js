608d22d03cfa5ff578c8ef7ea0bb34dab038e693
SanmithD <sanmithdevadiga7@gmail.com>
2025-03-09 10:29:04 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -8,27 +8,12 @@ const app = express();
 
 connectDB();
 app.use(
-    cors({
-      origin: function(origin, callback) {
-        const allowedOrigins = [
-          process.env.FRONT_URL,
-          'http://localhost:5173',
-          'https://vercel-test-frontend-delta.vercel.app/'
-        ];
-        
-        if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-          callback(null, true);
-        } else {
-          console.log("Blocked by CORS: ", origin);
-          callback(null, true); 
-        }
-      },
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      credentials: true,
-    })
-  );
+  cors({
+    origin: process.env.FRONT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/info", postRouter);
 
