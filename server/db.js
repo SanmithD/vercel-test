bcd3b866d4d972cb8fbfe73c6d175085fee2abd2
SanmithD <sanmithdevadiga7@gmail.com>
2025-03-09 09:46:52 +0530

    Update

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() =>{
     try {
-        const connectionDB = await mongoose.connect(process.env.MONGO_URI);
+        const connectionDB = await mongoose.connect(process.env.MONGO_URI || process.env.COMPASS );
         console.log(connectionDB.connection.host)
     } catch (error) {
         console.log(error)
